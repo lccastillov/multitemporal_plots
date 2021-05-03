766914767093cc1c6cbbbc3e6f1abbd315535147
Liliana <34029054+lccastillov@users.noreply.github.com>
2021-05-03 21:32:57 +0100

    Filled gaps.

--- Multi_temporal_monitoring.py
+++ Multi_temporal_monitoring.py
@@ -27,6 +27,14 @@
 ##
 ## --------------------------
 #################################################################
+## --------------------------
+## To run this script on Docker
+"""
+docker run -i -t -v /Users/lilianacastillo/Documents/Data_Analysis:/data lccastillov/histogram_analysis_v2   \
+python /data/Danper_ES/multitemporal_plots/Multi_temporal_monitoring.py
+"""
+
+## --------------------------
 ###### Import Functions #####
 
 import time
@@ -41,8 +49,126 @@ from numpy import genfromtxt
 import os
 import warnings
 
+from functions_multitemporal_monitoring import *
 
 
+# Switch warnings off
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+## ---------------------------
+
+## Declare variables
+m_root='/data'
+input_files_folder=m_root+'/Danper_ES/input_files'
+s2_vi_list = ['NDVI8'] # List of VIs to be assessed
+
+# Columns of dataframe that stores stats per day
+metrics_day_columns_df = [ 'age', 'vi', 'count', 'period_mean', 'period_median', 'std', 'low_thres', \
+                          'up_thres', 'IQR', 'Q1', 'Q3']
+daily_stats_df_total = pd.DataFrame( columns=metrics_day_columns_df)
+
+## ---------------------------
+
+## Read csv with field info per plot  into a pandas dataframe (df_age)
+plots_age_csv=input_files_folder+'/plots_age.csv'
+df_age=pd.read_csv(plots_age_csv)
+
+## ---------------------------
+## Data cleaning
+
+# Change fields data types
+df_age['Yield']=df_age['Yield'].astype(float)
+df_age['Month_Ini_Campana']=df_age['Month_Ini_Campana'].astype(int)
+df_age['age']=df_age['age'].astype(float)
+df_age['Ini_Campana'] = pd.to_datetime(df_age['Ini_Campana'])
+df_age['Fin_Campana'] = pd.to_datetime(df_age['Fin_Campana'])
+df_age['Image_date_dateformat'] = pd.to_datetime(df_age['Image_date'])
+
+# Filter using harvest date. i.g. Only images acquired before harvest date
+df_age=df_age[df_age['Image_date_dateformat']<=df_age['Fin_Campana']].copy()
+
+# Delete cosecha stage as the VIs are shown as very low values and can affect the final averages
+df_age=df_age[(df_age['Phenology']!= 'Harvest')&(df_age['Phenology']!= 'Pre-Harvest')]
+
+#Exclude any plot older than 50 days presented as being in sprouting (Sprouting does not occur at such ages
+drop_sprouting=df_age[(df_age['age']>60)&(df_age['Phenology']=='Sprouting')]
+indexNames =drop_sprouting.index
+df_age.drop(indexNames , inplace=True)
+
+## ---------------------------
+## Yield stats
+yield_min=df_age['Yield'].min()
+yield_max=df_age['Yield'].max()
+yield_median=df_age['Yield'].median()
+yield_mean=df_age['Yield'].mean()
+yield_std=df_age['Yield'].std()
+yield_count=df_age['Yield'].count()
+
+# Define thresholds to classify yields
+yield_low_threshold=yield_median-0*yield_std
+yield_high_threshold=yield_median+0.5*yield_std
+yield_high_threshold=yield_median-0.5*yield_std
+
+
+###################
+
+max_day = 140
+
+# Maximum week to retrieve statistics of VIs.
+# Identified as the latest week in which all plots have not been harvested yet
+# This, as the harvest might start eralier or later sometimes, this is the week in which,
+#for sure, al the plots are still in the filed (not harvested)
+
+max_week_campaign_duration=20
+min_vi = 0.3
+
+
+for vi in s2_vi_list:
+
+
+    hdf5_file = m_root + '/Danper_ES/input_files/SEN2_HistoMetrics' + vi + '.hdf5' #hdf file with VI stats per plot
+
+    #csv file that stores the result of the union of field data and VI stats per plot
+    csv_image_anomalies_vs_plots = \
+        m_root + '/Danper_LaLibertad/Anomalies_interpretation/Sen2_' + vi + '_csv_image_anomalies_vs_plots_v2.csv'
+
+    # Retrieve VI stats per plot from hdf5 file and merging them with field  data per plot
+    df, = import_hdf5(vi, df_age,hdf5_file,csv_image_anomalies_vs_plots)
+
+    #Maximum crop age to include in the stats
+    max_day = 150
+
+    df, = conditions_weeks(df) # Indicate the correspondent week for each day
+
+    days_list = np.arange(1, max_day + 1)
+
+    #############
+
+    # As we are creating the typical pattern, we include only the yields that are not outliers
+    df_nooutliers, = df_no_yield_outliers(df, yield_low_threshold, yield_high_threshold, min_vi= \
+        min_vi, max_week_campaign_duration=max_week_campaign_duration)
+
+    ############
+    periodicity = 'daily'
+    for day in days_list:
+        # delete  outliers per week based on the mean VI values per plot (i.e. mean vi)
+        column_name = 'mean_vi'
+        indexNames, = delete_outliers_iqr_day(df, column_name, day)
+        df.drop(indexNames, inplace=True)
+
+
+
+    # Stats of daily data without outliers (from yield and mean_vi perspective)
+    daily_stats_df_current, = daily_vi_statistics(df_nooutliers, max_day,vi,metrics_day_columns_df)
+
+
+    #Append stats for current day to main stats dataframe
+    daily_stats_df_total = daily_stats_df_total.append(daily_stats_df_current)
+
+    # When less that 20 records per day, assign Null to period median
+    daily_stats_df_total['period_median'] = \
+        np.where(daily_stats_df_total['count'] < 5, np.nan, daily_stats_df_total['period_median'])
+
+
+    metrics_df,=fill_gaps(metrics_df=daily_stats_df_total, periodicity='daily')
 
---
+++ functions_multitemporal_monitoring.py
@@ -0,0 +1,264 @@
+import pandas as pd
+import numpy as np
+def import_hdf5(VI, df_age, hdf5_file,csv_image_anomalies_vs_plots):
+    import pandas as pd
+    df = df_age
+    print("list of columns", list(df.columns))
+
+    # Read hdf file into pandas dataframe
+    hdf = pd.HDFStore(hdf5_file, mode='r')
+
+    # print("hdfkeys",hdf.keys()) #Shows all the keys within the hdf5 file
+
+    df_mean_vi = hdf.get('/PlotStats/PlotMeanVal')
+    df_norm_mean_vi = hdf.get('/NormalPlotStats/NormalMean')
+
+    # Retrieve percentage of Lower and upper anomalies per plot per date (from hdf5 file)
+    df_low_mean_vi = hdf.get('/LowPlotStats/low_Mean') #Percentage of low anomalies
+    df_up_mean_vi = hdf.get('/UpPlotStats/up_Mean')#Percentage of high anomalies
+    DF_numLowerAnomalies = hdf.get('/PlotStats/percLowAnomalies') #Number of pixels anomalous (low anomalies)
+    DF_numUpperAnomalies = hdf.get('/PlotStats/percUpperAnomalies')#Number of pixels anomalous (highanomalies)
+
+    # Create empty lists to store temporary lower and upper anomalies
+    mean_vi_Array = []
+    mean_normal_vi_array = []
+    mean_low_vi_array = []
+    mean_up_vi_array = []
+    lowerAnomalies = []
+    upperAnomalies = []
+
+    # Loop to go through all the indexes of the df and retrieve the respective percentage of anomalies
+    for index, row in df.iterrows():
+        plot = row['IDLote']
+        date = row['Image_date'].replace("-", "")
+
+        meanNDVI = df_mean_vi.loc[int(float(plot))][str(date)]
+        mean_normal_vi = df_norm_mean_vi.loc[int(float(plot))][str(date)]
+        low_mean_VI = df_low_mean_vi.loc[int(float(plot))][str(date)]
+        up_mean_VI = df_up_mean_vi.loc[int(float(plot))][str(date)]
+        numLowerAnomalies = DF_numLowerAnomalies.loc[int(float(plot))][str(date)]
+        numUpperAnomalies = DF_numUpperAnomalies.loc[int(float(plot))][str(date)]
+
+        # print("mean",meanNDVI, "low",low_mean_VI, "up_mean_VI",up_mean_VI,'numLowerAnomalies',numLowerAnomalies,"numUpperAnomalies", numUpperAnomalies)
+
+        # Show which of the plots are potentially clouded for specific dates
+        # if meanNDVI == -999:
+        # print("The plot", row['Codigo'], " is covered by clouds on ", row['Image_date'])
+
+        # Append the current values to the lists of lower and upper anomalies respectively
+        mean_vi_Array.append(meanNDVI)
+        lowerAnomalies.append(numLowerAnomalies)
+        upperAnomalies.append(numUpperAnomalies)
+        mean_normal_vi_array.append(mean_normal_vi)
+        mean_low_vi_array.append(low_mean_VI)
+        mean_up_vi_array.append(up_mean_VI)
+
+    # Create  new columns in the dataframe that will store the percentage of low and high anomalies
+    mean_vi_colum_title = 'mean_' + VI
+    df['mean_vi'] = mean_vi_Array
+    df['LowerAnomalies'] = lowerAnomalies
+    df['UpperAnomalies'] = upperAnomalies
+    df['normalmean_vi'] = mean_normal_vi_array
+    df['lowmean_vi'] = mean_low_vi_array
+    df['upmean_vi'] = mean_up_vi_array
+
+    # Remove the plots potentially clouded
+
+    # Remove plots with very low VI values (clouded)
+    df = df[df.mean_vi != -999]
+    df.to_csv(csv_image_anomalies_vs_plots, index=False)
+    return [df]
+
+
+
+def daily_vi_statistics(df, max_day,vi,metrics_day_columns_df):
+    import pandas as pd
+    day_list = np.arange(1, max_day + 1)
+
+
+    daily_stats_df = pd.DataFrame(index=day_list, \
+                                  columns=metrics_day_columns_df)
+
+    # Define indx
+    daily_stats_df.index.name = 'age'
+
+    #### First calculate exploratory statistics to remove outliers for each age (days)
+    for day in day_list:
+        # print("Estimating statistics for ", age, " days")
+
+        df_current_day = df[(df.age == day)].copy()
+
+        Q1 = df_current_day.normalmean_vi.quantile(0.25)
+
+        Q3 = df_current_day['normalmean_vi'].quantile(0.75)
+
+        IQR = Q3 - Q1
+        min_thres = Q1 - 1.5 * IQR
+        max_thres = Q3 + 1.5 * IQR
+        mean = df_current_day['normalmean_vi'].mean()
+        std = df_current_day['normalmean_vi'].std()
+        count = df_current_day['normalmean_vi'].count()
+        median = df_current_day['normalmean_vi'].median()
+
+        df_non_outliers = df_current_day
+        [(df_current_day.age >= min_thres) | (df_current_day.age <= max_thres)]
+        # df_non_outliers=df_current_age[(df_current_age.age >= min_thres) & df_current_age(df_current_age.age <= max_thres)]
+
+        daily_stats_df.loc[day]['age'] = day
+        daily_stats_df.loc[day]['count'] = count
+        daily_stats_df.loc[day]['period_mean'] = mean
+        daily_stats_df.loc[day]['std'] = std
+        daily_stats_df.loc[day]['low_thres'] = min_thres
+        daily_stats_df.loc[day]['up_thres'] = max_thres
+        daily_stats_df.loc[day]['IQR'] = IQR
+        daily_stats_df.loc[day]['Q1'] = Q1
+        daily_stats_df.loc[day]['Q3'] = Q3
+        daily_stats_df.loc[day]['vi'] = vi
+        daily_stats_df.loc[day]['period_median'] = median
+
+
+    # print("statistics_df_all_ages 124 function",statistics_df_all_ages)
+    # statistics_df_all_ages['age'] = statistics_df_all_ages['age'].astype(int)
+    # If we want to record statistics into a csv file
+    # statistics_df_all_ages.to_csv(m_root+'/Anomalies_interpretation/stats_'+s2_vi+'.csv', index=False)
+    return [daily_stats_df]
+
+def conditions_weeks (df):
+    #Retrieve number of week
+    #Check https://www.uaex.edu/publications/pdf/mp192/chapter-2.pdf
+    def f(row):
+        if row['age'] <= 7:
+            val = 1
+        elif row['age'] <= 14 :
+            val = 2
+        elif row['age'] <= 21:
+            val = 3
+        elif row['age'] <= 28:
+            val = 4
+        elif row['age'] <= 35:
+            val = 5
+        elif row['age'] <= 42:
+            val = 6
+        elif row['age'] <= 49:
+            val = 7
+        elif row['age'] <= 56:
+            val = 8
+        elif row['age'] <= 63:
+            val = 9
+        elif row['age'] <= 70:
+            val = 10
+        elif row['age'] <= 77:
+            val = 11
+        elif row['age'] <= 84:
+            val = 12
+        elif row['age'] <= 56:
+            val = 13
+        elif row['age'] <= 56:
+            val = 14
+        elif row['age'] <= 42:
+            val = 6
+        elif row['age'] <= 49:
+            val = 7
+        elif row['age'] <= 56:
+            val = 8
+        elif row['age'] <= 63:
+            val = 9
+        elif row['age'] <= 70:
+            val = 10
+        elif row['age'] <= 77:
+            val = 11
+        elif row['age'] <= 84:
+            val = 12
+        elif row['age'] <= 91:
+            val = 13
+        elif row['age'] <= 98:
+            val = 14
+        elif row['age'] <= 105:
+            val = 15
+        elif row['age'] <= 112:
+            val = 16
+        elif row['age'] <= 119:
+            val = 17
+        elif row['age'] <= 126:
+            val = 18
+        elif row['age'] <= 133:
+            val = 19
+        elif row['age'] <= 140:
+            val = 20
+        elif row['age'] <= 147:
+            val = 21
+        elif row['age'] <= 154:
+            val = 22
+        elif row['age'] <= 161:
+            val = 23
+        elif row['age'] <= 168:
+            val = 24
+        elif row['age'] <= 175:
+            val = 25
+        elif row['age'] <= 182:
+            val = 26
+        else:
+            val = 26
+
+        return val
+
+    df['week_campaign']=df.apply(f, axis=1)
+    return[df]
+
+
+def df_no_yield_outliers(df, yield_low_threshold, yield_high_threshold, min_vi, max_week_campaign_duration):
+    # This function drops the data associated to those plots which yield is an outlier based on the interquartile \
+    # range outlier criteria
+    import numpy as np
+    # create emtpy df
+    df["yield_type"] = ""
+
+    df["yield_type"] = np.where(df['Yield'] <= yield_low_threshold, \
+                                "low yield", df['yield_type']).copy()
+    df["yield_type"] = np.where(df['Yield'] > yield_high_threshold, \
+                                "high_yield", df['yield_type'])
+
+    # Only high yields
+    df = df[df['yield_type'] == 'high_yield'].copy()
+
+    # Delete weird values (lower then NDvi 0.3 in weeks highern than 21)
+    # this max week refers to the maximum expected week before harvesing
+
+    drop_outliers = df[(df['week_campaign'] >= max_week_campaign_duration) & (df['mean_vi'] < min_vi)]
+    indexNames = drop_outliers.index
+    df.drop(indexNames, inplace=True)
+    return [df]
+
+def delete_outliers_iqr_day (df, column_name, day):
+    current_day_df=df[df['age']==day]
+    Q1 = current_day_df[column_name].quantile(0.25)
+
+    Q3 = current_day_df[column_name].quantile(0.75)
+
+    IQR = Q3 - Q1
+    upper_outlier_threshold=Q3 + 1.5 * IQR
+    lower_outlier_threshold=Q1-1.5*IQR
+
+    df_drop = df[((df[column_name] <= (Q1 - 1.5 * IQR))&(df['week_campaign'] == day))|\
+                 ((df[column_name] >= (Q3 + 1.5 * IQR))&(df['week_campaign'] == day))]
+    indexNames =df_drop.index
+
+    return [indexNames]
+
+def fill_gaps(metrics_df, periodicity):
+    if periodicity == "weekly":
+        metrics_df = metrics_df.set_index('week_campaign')
+    elif periodicity == "daily":
+        metrics_df = metrics_df.set_index('age')
+
+    # Filling using mean or median
+    # Creating a column in the dataframe
+    # instead of : df['NewCol']=0, we use
+    # df = df.assign(NewCol=default_value)
+    # to avoid pandas warning.
+
+    metrics_df = metrics_df.assign(fill_mean=metrics_df.period_median.fillna(metrics_df.period_median. \
+                                                                             rolling(7, min_periods=1, ).mean()))
+    #min_periods: Minimum number of observations in window required to have a value
+    return[metrics_df]
+
